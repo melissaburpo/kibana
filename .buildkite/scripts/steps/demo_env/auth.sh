be3edecd44dc59b5159cdff3295a04841ecae2ff
Brian Seeders <brian.seeders@elastic.co>
2021-08-13 14:57:28 -0400

    Switch to new demo GKE cluster

--- .buildkite/scripts/steps/demo_env/auth.sh
+++ .buildkite/scripts/steps/demo_env/auth.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 echo '--- Auth and set up kubectl'
 
-gcloud container clusters get-credentials public-kibana --region us-central1 --project elastic-kibana-184716
-kubectl config use-context gke_elastic-kibana-184716_us-central1_public-kibana
+gcloud container clusters get-credentials demo-env --region us-central1 --project elastic-kibana-184716
+kubectl config use-context gke_elastic-kibana-184716_us-central1_demo-env
